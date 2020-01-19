96717ff6b11a69859ff6f1b4cc701295e016be4c
aaronjparsons <aaron.parsons@live.ca>
2020-01-18 19:23:42 -0700

    Add Buy Coffee button

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list nav>
-        <v-list-item v-for="(item, index) in navItems" :key="index" link :to="item.route">
+        <v-list-item
+          v-for="(item, index) in navItems"
+          :key="index"
+          link
+          :to="item.route"
+        >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -15,13 +16,26 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <template v-slot:append>
+        <v-row class="pb-4" justify="center">
+          <a
+            class="bmc-button"
+            target="_blank"
+            href="https://www.buymeacoffee.com/ChewyDinosaur"
+          >
+            <img
+              src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+              alt="Buy me a coffee"
+            />
+            <span style="margin-left:15px;font-size:28px !important;">
+              Buy me a coffee
+            </span>
+          </a>
+        </v-row>
+      </template>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      clipped-left
-      class="grey darken-4"
-    >
+    <v-app-bar app clipped-left class="grey darken-4">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>EFT Field Guide</v-toolbar-title>
     </v-app-bar>
@@ -43,51 +57,94 @@ query {
 </static-query>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
+export default {
+  props: {
+    source: String
+  },
 
-    data: () => ({
-      drawer: null,
-      navItems: [
-        {
-          route: '/',
-          icon: 'mdi-view-dashboard',
-          label: 'Home'
-        },
-        {
-          route: '/keyspawns',
-          icon: 'mdi-key',
-          label: 'Key Spawns'
-        },
-        {
-          route: '/extractions',
-          icon: 'mdi-map',
-          label: 'Extractions'
-        },
-        {
-          route: '/ammo',
-          icon: 'mdi-poll-box',
-          label: 'Ammo Chart'
-        },
-        {
-          route: '/checklists',
-          icon: 'mdi-checkbox-marked',
-          label: 'Checklists'
-        },
-        {
-          route: '/resources',
-          icon: 'mdi-folder',
-          label: 'More Resources'
-        }
-      ]
-    }),
+  data: () => ({
+    drawer: null,
+    navItems: [
+      {
+        route: '/',
+        icon: 'mdi-view-dashboard',
+        label: 'Home'
+      },
+      {
+        route: '/keyspawns',
+        icon: 'mdi-key',
+        label: 'Key Spawns'
+      },
+      {
+        route: '/extractions',
+        icon: 'mdi-map',
+        label: 'Extractions'
+      },
+      {
+        route: '/ammo',
+        icon: 'mdi-poll-box',
+        label: 'Ammo Chart'
+      },
+      {
+        route: '/checklists',
+        icon: 'mdi-checkbox-marked',
+        label: 'Checklists'
+      },
+      {
+        route: '/resources',
+        icon: 'mdi-folder',
+        label: 'More Resources'
+      }
+    ]
+  }),
 
-    created () {
-      this.$vuetify.theme.dark = true
-    },
+  created() {
+    this.$vuetify.theme.dark = true
   }
+}
 </script>
 
-<style></style>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Cookie&display=swap');
+
+.bmc-button img {
+  width: 35px !important;
+  margin-bottom: 1px !important;
+  box-shadow: none !important;
+  border: none !important;
+  vertical-align: middle !important;
+}
+.bmc-button {
+  padding: 7px 10px 7px 10px !important;
+  line-height: 35px !important;
+  height: 51px !important;
+  min-width: 217px !important;
+  text-decoration: none !important;
+  display: inline-flex !important;
+  color: #ffffff !important;
+  background-color: #212121 !important;
+  border-radius: 5px !important;
+  border: 1px solid transparent !important;
+  padding: 7px 10px 7px 10px !important;
+  font-size: 22px !important;
+  letter-spacing: 0.6px !important;
+  margin: 0 auto !important;
+  font-family: 'Cookie', cursive !important;
+  -webkit-box-sizing: border-box !important;
+  box-sizing: border-box !important;
+  -o-transition: 0.3s all linear !important;
+  -webkit-transition: 0.3s all linear !important;
+  -moz-transition: 0.3s all linear !important;
+  -ms-transition: 0.3s all linear !important;
+  transition: 0.3s all linear !important;
+}
+.bmc-button:hover,
+.bmc-button:active,
+.bmc-button:focus {
+  -webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;
+  text-decoration: none !important;
+  box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;
+  opacity: 0.85 !important;
+  color: #ffffff !important;
+}
+</style>
