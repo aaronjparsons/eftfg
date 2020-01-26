5fea7d83514c35acc3b3691d9e9995c939464c64
aaronjparsons <aaron.parsons@live.ca>
2020-01-26 15:09:31 -0700

    Break Map out into its own component. Complete media dialogs and all map elements

---
+++ src/components/Map.vue
@@ -0,0 +1,226 @@
+<template>
+  <div :style="{ height: '65vh' }">
+    <l-map
+      :zoom="mapOptions.zoom"
+      :minZoom="mapOptions.zoom"
+      :maxZoom="mapOptions.maxZoom"
+      :center="center"
+      :crs="mapOptions.crs"
+    >
+      <div class="map-name">{{ activeMap }}</div>
+      <l-tile-layer :url="mapSource"></l-tile-layer>
+      <l-feature-group ref="markerGroup">
+        <l-marker
+          v-for="(coord, index) in activeItem.coords"
+          :key="index"
+          :lat-lng="coord"
+        >
+          <l-popup>
+            <h3>{{ activeItem.name[index] }}</h3>
+            <p>Type: {{ activeItem.type[index] }}</p>
+            <p>Notes: {{ activeItem.notes[index] }}</p>
+            <v-btn
+              :class="{
+                strikethrough: !hasMediaContent(activeItem.images, index)
+              }"
+              color="grey darken-4"
+              @click.stop="openDialog('image', activeItem.images[index])"
+              :disabled="!hasMediaContent(activeItem.images, index)"
+            >
+              Screenshot
+            </v-btn>
+            <v-btn
+              class="ml-2"
+              :class="{
+                strikethrough: !hasMediaContent(activeItem.videos, index)
+              }"
+              color="grey darken-4"
+              @click.stop="openDialog('video', activeItem.videos[index])"
+              :disabled="!hasMediaContent(activeItem.videos, index)"
+            >
+              Video Guide
+            </v-btn>
+          </l-popup>
+        </l-marker>
+      </l-feature-group>
+    </l-map>
+    <v-dialog v-model="popupDialog" width="750">
+      <div class="image-dialog-container">
+        <div class="icon-container">
+          <v-icon @click="closeDialog">mdi-close</v-icon>
+        </div>
+        <v-img v-if="imageSource" :src="imageSource" contain />
+        <iframe
+          v-if="videoSource"
+          :height="iframeHeight()"
+          width="100%"
+          :src="`https://www.youtube.com/embed/${videoSource}`"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    activeMap: {
+      type: String,
+      default: ''
+    },
+    activeItem: {
+      type: Object,
+      default: () => {}
+    }
+  },
+
+  data() {
+    return {
+      popupDialog: false,
+      imageSource: null,
+      videoSource: null,
+      mapOptions: {
+        zoom: 2,
+        maxZoom: 5,
+        crs: L.CRS.Simple
+      }
+    }
+  },
+
+  computed: {
+    mapSource() {
+      return `mapimages/${this.activeMap}/{z}/{x}/{y}.png`;
+    },
+    center() {
+      if (this.activeItem.coords.length > 0) {
+        const markersLength = this.activeItem.coords.length;
+        let lats = 0;
+        let lngs = 0;
+        this.activeItem.coords.map(marker => {
+          lats += marker[0];
+          lngs += marker[1];
+        });
+        return [lats / markersLength, lngs / markersLength];
+      } else {
+        return [0, 0];
+      }
+    }
+  },
+
+  methods: {
+    hasMediaContent(value, index) {
+      if (!value) {
+        return false
+      }
+      return value[index].length > 0
+    },
+    iframeHeight() {
+      const windowHeight = window.innerHeight;
+      return windowHeight * 0.8;
+    },
+    openDialog(type, source) {
+      if (type === 'image') {
+        this.imageSource = source
+      } else {
+        this.videoSource = source
+      }
+      this.popupDialog = true
+    },
+    closeDialog() {
+      this.imageSource = null
+      this.videoSource = null
+      this.popupDialog = false
+    }
+  }
+}
+</script>
+
+<style>
+.map-name {
+  position: absolute;
+  right: 0;
+  padding: 5px 20px;
+  background: #434343d8;
+  border-bottom-left-radius: 5px;
+  font-size: 16px;
+  z-index: 150;
+}
+.image-dialog-container {
+  position: relative;
+}
+.icon-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+  color: white;
+  z-index: 250;
+  transition: all 300ms;
+  background: #424242d0;
+  border-radius: 20px;
+  padding: 5px;
+}
+.icon-container:hover {
+  transform: scale(1.2);
+}
+.icon-container i {
+  font-size: 30px;
+}
+
+.strikethrough {
+  text-decoration: line-through;
+}
+.theme--dark.v-btn.v-btn--disabled:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+  background: #313131 !important;
+}
+
+.leaflet-container {
+  background-color: #424242;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  position: relative;
+  border-radius: 4px;
+}
+
+/* Leaflet z-index overrides */
+.leaflet-pane {
+  z-index: 110 !important;
+} /* 400 */
+.leaflet-tile-pane {
+  z-index: 105 !important;
+} /* 200 */
+.leaflet-overlay-pane {
+  z-index: 110 !important;
+} /* 400 */
+.leaflet-shadow-pane {
+  z-index: 115 !important;
+} /* 500 */
+.leaflet-marker-pane {
+  z-index: 120 !important;
+} /* 600 */
+.leaflet-tooltip-pane {
+  z-index: 125 !important;
+} /* 650 */
+.leaflet-popup-pane {
+  z-index: 130 !important;
+} /* 700 */
+.leaflet-map-pane canvas {
+  z-index: 100 !important;
+} /* 100 */
+.leaflet-map-pane svg {
+  z-index: 105 !important;
+} /* 200 */
+.leaflet-control {
+  z-index: 135 !important; /* 800 */
+}
+.leaflet-top,
+.leaflet-bottom {
+  z-index: 150 !important; /* 1000 */
+}
+</style>
--- src/pages/Extractions.vue
+++ src/pages/Extractions.vue
@@ -13,37 +13,8 @@
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-col cols="12" sm="10" :style="{ height: '65vh' }">
-        <l-map
-          :zoom="mapOptions.zoom"
-          :minZoom="mapOptions.zoom"
-          :maxZoom="mapOptions.maxZoom"
-          :center="center"
-          :crs="mapOptions.crs"
-        >
-          <!-- <div class="map-name">{{ activeMapName }}</div> -->
-          <l-tile-layer :url="activeMap"></l-tile-layer>
-          <l-feature-group ref="markerGroup">
-            <l-marker
-              v-for="(coord, index) in activeItem.coords"
-              :key="index"
-              :lat-lng="coord"
-            >
-              <l-popup>
-                <h3>{{ activeItem.name[index] }}</h3>
-                <p>Type: {{ activeItem.type[index] }}</p>
-                <p>Notes: {{ activeItem.notes[index] }}</p>
-                <!-- <v-btn
-                  v-if="hasMediaContent(activeItem.images[index])"
-                  dark
-                  @click="openDialog('image', activeItem.images[index])"
-                  :disabled="!hasMediaContent(activeItem.images[index])"
-                  >Screenshot</v-btn
-                > -->
-              </l-popup>
-            </l-marker>
-          </l-feature-group>
-        </l-map>
+      <v-col cols="12" sm="10">
+        <Map :active-map="activeMap" :active-item="activeItem" />
       </v-col>
     </v-row>
   </Layout>
@@ -167,15 +138,15 @@ query {
 </page-query>
 
 <script>
+import Map from '../components/Map'
 export default {
+  components: {
+    Map
+  },
+
   data() {
     return {
-      input: {},
-      mapOptions: {
-        zoom: 2,
-        maxZoom: 5,
-        crs: L.CRS.Simple
-      }
+      input: {}
     }
   },
 
@@ -217,23 +188,9 @@ export default {
     },
     activeMap() {
       if (this.input.node) {
-        return `mapimages/${this.input.node.map}/{z}/{x}/{y}.png`;
+        return this.input.node.map
       } else {
-        return "";
-      }
-    },
-    center() {
-      if (this.activeItem.coords.length > 0) {
-        const markersLength = this.activeItem.coords.length;
-        let lats = 0;
-        let lngs = 0;
-        this.activeItem.coords.map(marker => {
-          lats += marker[0];
-          lngs += marker[1];
-        });
-        return [lats / markersLength, lngs / markersLength];
-      } else {
-        return [0, 0];
+        return ''
       }
     }
   }
@@ -243,49 +200,5 @@ export default {
 <style>
 .v-autocomplete {
   z-index: 160;
-}
-
-.leaflet-container {
-  background-color: #424242;
-  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-  position: relative;
-  border-radius: 4px;
-}
-
-/* Leaflet z-index overrides */
-.leaflet-pane {
-  z-index: 110 !important;
-} /* 400 */
-.leaflet-tile-pane {
-  z-index: 105 !important;
-} /* 200 */
-.leaflet-overlay-pane {
-  z-index: 110 !important;
-} /* 400 */
-.leaflet-shadow-pane {
-  z-index: 115 !important;
-} /* 500 */
-.leaflet-marker-pane {
-  z-index: 120 !important;
-} /* 600 */
-.leaflet-tooltip-pane {
-  z-index: 125 !important;
-} /* 650 */
-.leaflet-popup-pane {
-  z-index: 130 !important;
-} /* 700 */
-.leaflet-map-pane canvas {
-  z-index: 100 !important;
-} /* 100 */
-.leaflet-map-pane svg {
-  z-index: 105 !important;
-} /* 200 */
-.leaflet-control {
-  z-index: 135 !important; /* 800 */
-}
-.leaflet-top,
-.leaflet-bottom {
-  z-index: 150 !important; /* 1000 */
 }
 </style>
