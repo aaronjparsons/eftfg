7f67341d97f5cac14b437cc5e6d16d560ea77cc6
aaronjparsons <aaron.parsons@live.ca>
2020-11-29 12:51:49 -0700

    Finish caches

--- src/pages/Caches.vue
+++ src/pages/Caches.vue
@@ -169,7 +169,7 @@ export default {
 .theme--dark.v-text-field--solo > .v-input__control > .v-input__slot {
   background-color: #161616;
 }
-.v-autocomplete {
+.v-menu__content {
   z-index: 160 !important;
 }
 .theme--dark.v-list {
